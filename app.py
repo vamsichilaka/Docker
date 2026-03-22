dac29857b1564a11c689d8ba2de0dcf65b708776
vamsichilaka <168081361+vamsichilaka@users.noreply.github.com>
2026-03-22 15:44:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,16 @@
-import os
-import redis
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 PORT = 5000
 
-redis_host = os.getenv("REDIS_HOST", "localhost")
-r = redis.Redis(host=redis_host, port=6379)
-
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        count = r.incr("visits")
-
-        message = f"Build Version Running | Visits: {count}"
-
+        message = "Build Version Running | Webserver Active"
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
-
         self.wfile.write(message.encode())
 
-with HTTPServer(("", PORT), Handler) as httpd:
-    httpd.serve_forever()
+if __name__ == "__main__":
+    print(f"Server running on port {PORT}...")
+    with HTTPServer(("", PORT), Handler) as httpd:
+        httpd.serve_forever()
